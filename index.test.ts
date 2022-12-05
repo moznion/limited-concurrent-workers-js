b4424ea1b02a2ea2e8b2f29f03b8628625f8005b
moznion <moznion@mail.moznion.net>
2022-12-04 16:35:38 -0800

    Implement the fundamental functions
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ index.test.ts
@@ -0,0 +1,150 @@
+import { describe, expect, test } from '@jest/globals';
+import { runConcurrent, runConcurrentFlatten, runConcurrentSettled } from './index';
+
+describe('runConcurrent', () => {
+  test('it executes successfully by three workers', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    const result = await runConcurrent<string, number>(
+      3,
+      arg => (resolve, reject) => {
+        resolve(Number(arg));
+      },
+      generator,
+    );
+    expect(result.length).toBe(3);
+    expect(result[0]?.length).toBeGreaterThan(0);
+    expect(result[1]?.length).toBeGreaterThan(0);
+    expect(result[2]?.length).toBeGreaterThan(0);
+    expect(result.flat().reduce((acc, i) => acc + i, 0)).toBe(4950); // sum of all numbers from 0 to 99
+  });
+
+  test('a worker rejects', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    try {
+      await runConcurrent<string, number>(
+        3,
+        arg => (resolve, reject) => {
+          reject('reject!');
+        },
+        generator,
+      );
+      expect(true).toBe(false); // must not reach here
+    } catch (err) {
+      expect(err).toBe('reject!');
+    }
+  });
+});
+
+describe('runConcurrentFlatten', () => {
+  test('it executes successfully by three workers', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    const result = await runConcurrentFlatten<string, number>(
+      3,
+      arg => (resolve, reject) => {
+        resolve(Number(arg));
+      },
+      generator,
+    );
+    expect(result.length).toBe(100);
+    expect(result.reduce((acc, i) => acc + i, 0)).toBe(4950); // sum of all numbers from 0 to 99
+  });
+
+  test('a worker rejects', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    try {
+      await runConcurrentFlatten<string, number>(
+        3,
+        arg => (resolve, reject) => {
+          reject('reject!');
+        },
+        generator,
+      );
+      expect(true).toBe(false); // must not reach here
+    } catch (err) {
+      expect(err).toBe('reject!');
+    }
+  });
+});
+
+describe('runConcurrentSettled', () => {
+  test('it executes successfully by three workers', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    const result = await runConcurrentSettled<string, number>(
+      3,
+      arg => (resolve, reject) => {
+        resolve(Number(arg));
+      },
+      generator,
+    );
+    expect(result.length).toBe(3);
+    expect(result[0]?.status).toBe('fulfilled');
+    expect(result[1]?.status).toBe('fulfilled');
+    expect(result[2]?.status).toBe('fulfilled');
+    expect(result.flatMap(r => (r as PromiseFulfilledResult<number[]>).value).reduce((acc, i) => acc + i, 0)).toBe(
+      4950,
+    ); // sum of all numbers from 0 to 99
+  });
+
+  test('a worker rejects', async () => {
+    const generator = function* (): IterableIterator<string> {
+      for (let i = 0; i < 100; i++) {
+        yield i.toString();
+      }
+    };
+
+    const result = await runConcurrentSettled<string, number>(
+      3,
+      arg => (resolve, reject) => {
+        if (arg === '0') {
+          reject('reject!');
+          return;
+        }
+        resolve(Number(arg));
+      },
+      generator,
+    );
+    expect(result.length).toBe(3);
+
+    let fulfilledResults: PromiseFulfilledResult<number[]>[] = [];
+    let numOfRejected = 0;
+    let rejectedResult;
+    for (const r of result) {
+      if (r.status === 'fulfilled') {
+        fulfilledResults.push(r);
+        continue;
+      }
+      numOfRejected++;
+      rejectedResult = r;
+    }
+
+    expect(numOfRejected).toBe(1);
+    expect(rejectedResult?.reason).toBe('reject!');
+    expect(fulfilledResults.length).toBe(2);
+    expect(fulfilledResults.flatMap(r => r.value).reduce((acc, i) => acc + i, 0)).toBe(4950); // sum of all numbers from 1 to 99
+  });
+});
---
+++ index.ts
@@ -0,0 +1,107 @@
+/**
+ * runConcurrent runs executors that come from `executorProvider` by at most `maxConcurrency` workers.
+ * The workers continue to run the executors until the generator that is supplied by `generatorFunction` notifies `done`, i.e. once the generator has become `done`.
+ *
+ * @typeParam T - The argument type of the executor. That argument must be provided by the generator.
+ * @typeParam R - The result type of the executor, and propagates that result to this function's Promise.
+ * @param maxConcurrency - the number of the maximum concurrent workers.
+ * @param executorProvider - a provider function that returns an executor for a worker according to the `T` typed value that comes from the generator.
+ * @param generatorFunction - a generator function that returns an argument for `executorProvider`. If this generator returns `done` value, this function finishes the workers' executions and returns the result.
+ * @returns a promise that returns the arrays of `R` for each worker.
+ */
+export async function runConcurrent<T, R>(
+  maxConcurrency: number,
+  executorProvider: (
+    arg: T,
+  ) => (resolve: (value: R | PromiseLike<R>) => void, reject: (reason?: unknown) => void) => void,
+  generatorFunction: () => IterableIterator<T>,
+): Promise<Awaited<R[][]>> {
+  const promises = doRunConcurrent(maxConcurrency, executorProvider, generatorFunction);
+  return Promise.all(promises);
+}
+
+/**
+ * runConcurrentFlatten runs executors that come from `executorProvider` by at most `maxConcurrency` workers.
+ * The workers continue to run the executors until the generator that is supplied by `generatorFunction` notifies `done`, i.e. once the generator has become `done`.
+ *
+ * @typeParam T - The argument type of the executor. That argument must be provided by the generator.
+ * @typeParam R - The result type of the executor, and propagates that result to this function's Promise.
+ * @param maxConcurrency - the number of the maximum concurrent workers.
+ * @param executorProvider - a provider function that returns an executor for a worker according to the `T` typed value that comes from the generator.
+ * @param generatorFunction - a generator function that returns an argument for `executorProvider`. If this generator returns `done` value, this function finishes the workers' executions and returns the result.
+ * @returns a promise that returns an array of `R` that is flattened of all worker's results.
+ */
+export async function runConcurrentFlatten<T, R>(
+  maxConcurrency: number,
+  executorProvider: (
+    arg: T,
+  ) => (resolve: (value: R | PromiseLike<R>) => void, reject: (reason?: unknown) => void) => void,
+  generatorFunction: () => IterableIterator<T>,
+): Promise<Awaited<R[]>> {
+  const promises = doRunConcurrent(maxConcurrency, executorProvider, generatorFunction);
+  return new Promise((resolve, reject) => {
+    Promise.all(promises)
+      .then(results => {
+        resolve(results.flat());
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+/**
+ * runConcurrentSettled runs executors that come from `executorProvider` by at most `maxConcurrency` workers.
+ * The workers continue to run the executors until the generator that is supplied by `generatorFunction` notifies `done`, i.e. once the generator has become `done`.
+ *
+ * @typeParam T - The argument type of the executor. That argument must be provided by the generator.
+ * @typeParam R - The result type of the executor, and propagates that result to this function's Promise.
+ * @param maxConcurrency - the number of the maximum concurrent workers.
+ * @param executorProvider - a provider function that returns an executor for a worker according to the `T` typed value that comes from the generator.
+ * @param generatorFunction - a generator function that returns an argument for `executorProvider`. If this generator returns `done` value, this function finishes the workers' executions and returns the result.
+ * @returns a promise of the `Promise.allSettled()` that returns `R[]` result.
+ */
+export async function runConcurrentSettled<T, R>(
+  maxConcurrency: number,
+  executorProvider: (
+    arg: T,
+  ) => (resolve: (value: R | PromiseLike<R>) => void, reject: (reason?: unknown) => void) => void,
+  generatorFunction: () => IterableIterator<T>,
+): Promise<PromiseSettledResult<R[]>[]> {
+  const promises = doRunConcurrent(maxConcurrency, executorProvider, generatorFunction);
+  return Promise.allSettled(promises);
+}
+
+function doRunConcurrent<T, R>(
+  maxConcurrency: number,
+  executorProvider: (
+    arg: T,
+  ) => (resolve: (value: R | PromiseLike<R>) => void, reject: (reason?: unknown) => void) => void,
+  generatorFunction: () => IterableIterator<T>,
+): Promise<R[]>[] {
+  const promises: Promise<R[]>[] = [];
+  const generator = generatorFunction();
+
+  for (let i = 0; i < maxConcurrency; i++) {
+    const p = new Promise<R[]>(async (resolve, reject) => {
+      // eslint-disable-line no-async-promise-executor
+      const results: R[] = [];
+
+      let gen = generator.next();
+      while (!gen.done) {
+        try {
+          results.push(await new Promise(executorProvider(gen.value)));
+        } catch (e) {
+          reject(e);
+          return;
+        }
+        gen = generator.next();
+      }
+
+      resolve(results);
+    });
+    promises.push(p);
+  }
+
+  return promises;
+}
